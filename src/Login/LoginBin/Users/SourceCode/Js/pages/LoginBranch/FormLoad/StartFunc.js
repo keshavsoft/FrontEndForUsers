afa19c795e28274e926658b2f1c98a5dabfc37dd
Srivasu215 <sri.vasu215@gmail.com>
2025-03-13 13:23:23 +0530

    Branch For login

--- src/Login/LoginBin/Users/SourceCode/Js/pages/LoginBranch/FormLoad/StartFunc.js
+++ src/Login/LoginBin/Users/SourceCode/Js/pages/LoginBranch/FormLoad/StartFunc.js
@@ -1,13 +1,9 @@
 import { StartFunc as StartFuncAddListeners } from "./AddListeners/StartFunc.js";
+import { StartFunc as Delete } from "./Delete.js";
 
 const StartFunc = () => {
-    LocalDeleteCookie();
+    Delete();
     StartFuncAddListeners();
-};
-
-let LocalDeleteCookie = () => {
-    let lcoalKTokenId = "KSToken";
-    document.cookie = `${lcoalKTokenId}=; expires=Thu, 01 Jan 1947 00:00:00 UTC; path=/;`;
 };
 
 export { StartFunc };
